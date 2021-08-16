76aab5fb805c81cf90b24b95266726c490ec991a
AymenBarka <aymenbarka@yahoo.fr>
2021-08-16 12:12:15 +0200

    responsive side bar with angular material

---
+++ responsiveAngular/src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { delay } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private observer: BreakpointObserver) { }
+  @ViewChild(MatSidenav,{static:false})
+  sidenav!: MatSidenav;
+  ngOnInit() {
+  }
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+
+}
+}
